23039d6c36e4b739b52761e0c5b605227c5ab04a
MaiAbdElmonem <maiabdelmonem278@gmail.com>
2021-02-02 13:29:41 +0200

    swift ui sample almost done except adding user image

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -9,13 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      
+      ZStack(alignment: .top) {// ContainerView
+        Color("ghostWhite")
+        
+        ZStack {
+        Image("Mask").resizable().frame(maxWidth: .infinity, maxHeight: 220).ignoresSafeArea()
+        }
+        Spacer().frame(height: 200)
+        ScrollView(.vertical, showsIndicators: false) {
+        VStack {
+           HStack {
+           Button(action: {
+            print("button pressed")
+           }) {
+            Image("ic_edit").renderingMode(.original)
+           }.frame(width: 22, height: 23)
+          .padding(.leading, 20)
+            
+          Spacer()
+            
+          Button(action: {
+            print("button pressed")
+          }) {
+            Image("ic_back").renderingMode(.original)
+          }.frame(width: 22, height: 23)
+            .padding(.trailing, 13)
+        }
+           .padding(.top, 20)
+           
+          HStack(spacing: 8) {
+          Spacer()
+          Text("معلوماتك").foregroundColor(.white)
+            Divider().frame(width: 4, height: 23).cornerRadius(2).background(Color.init("divider")).padding(.trailing, 18)
+          }.padding(.top, 18)
+          
+          Spacer().frame(height: 30)
+            
+//          ScrollView(.vertical, showsIndicators: false) {
+            
+          VStack(alignment:.center) {
+                         
+              VStack(alignment: .trailing) {
+                UserInfo()
+                Divider().frame(height: 0.5).background(Color.init("CadetGrey"))
+                  .padding(.horizontal, 14)
+                  .padding(.top, 20)
+              }
+              
+              VStack(alignment: .trailing, spacing: 18) {
+                Text("نبذة عنك").foregroundColor(Color.init("SteelBlue"))
+                Text("هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً")
+                  .foregroundColor(Color.init("AuroMetalSaurus"))
+                  .multilineTextAlignment(.trailing).lineSpacing(5)
+              }
+              .padding(.trailing, 12)
+              .padding(.top, 14)
+              .padding(.leading, 16)
+              .padding(.bottom, 15)
+
+            }.background(Color.white).cornerRadius(16)
+            .padding(.bottom, 43)
+//                          ZStack(alignment: .top) {
+//
+//                            Image("ic_avatar-bg").resizable().frame(width: 100, height: 100, alignment: .center)
+//                          }.offset(y: -684)
+          }
+          .padding(.horizontal, 15)
+        }
+        
+      }
+      
     }
+  
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+                     ContentView()
+                          .previewDevice(PreviewDevice(rawValue: deviceName))
+                          .previewDisplayName(deviceName)
+                }
     }
 }
+#endif
--- SwiftUISample/UserInfo.swift
+++ SwiftUISample/UserInfo.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct UserInfo: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+//  let title: String
+//  let subTitle: String
+//
+//  init(title: String, subTitle: String) {
+//      self.title = title
+//      self.subTitle = subTitle
+//  }
+  var body: some View {
+    
+    HStack(alignment: .center, spacing: 28) {
+      VStack(alignment: .trailing, spacing: 30) {
+        Text("محمد أحمد")
+        Text("فكرة")
+        Text("التقنية")
+        Text("تطبيقات الجوال")
+        Text("٢٠١٩")
+      }.foregroundColor(Color.init("SteelBlue"))
+      
+      VStack(alignment: .trailing, spacing: 30) {
+        Text("صاحب المنشأة")
+        Text("نوع المنشأة")
+        Text("القطاع")
+        Text("تخصص المنشأة")
+        Text("تاريخ التأسيس")
+      }.foregroundColor(Color.init("DarkSlateGray"))
+     }
+    
+     .padding(.top, 112)
+    .padding(.leading, 15)
+    .padding(.trailing, 15)
+  }
 }
 
 struct UserInfo_Previews: PreviewProvider {
